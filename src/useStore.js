3b39a4191f1530f72662c4053f0fc723acbda801
Gautam Rakholiya <112694190+GautamRakholiya-1302@users.noreply.github.com>
2024-07-19 01:37:05 +0530

    Create useStore.js

---
+++ src/useStore.js
@@ -0,0 +1,11 @@
+// src/useStore.js
+import create from 'zustand';
+
+// Create a Zustand store
+const useStore = create((set) => ({
+  count: 0,
+  increaseCount: () => set((state) => ({ count: state.count + 1 })),
+  decreaseCount: () => set((state) => ({ count: state.count - 1 })),
+}));
+
+export default useStore;
